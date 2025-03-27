de2cf3023940fc9b74e7ed8791c2dbd5269dd438
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2025-03-27 00:34:16 +0000

    new user resource for api

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,11 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Resources\UserResource;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/user', function (Request $request) {
-        return $request->user();
+        return new UserResource($request->user()->load('socialLinks'));
     });
 });
