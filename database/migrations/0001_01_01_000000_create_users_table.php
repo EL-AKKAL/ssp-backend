f6a6ac44f558d01116ae985eb20d01a27952bfa1
EL AKKAL AYOUB <akkalayoub3@gmail.com>
2025-03-30 23:24:31 +0000

    fix(registration): make function field nullable to prevent database errors

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,9 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('function');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['user', 'admin'])->default('user');
+            $table->string('function')->nullable();
             $table->string('phone')->nullable();
             $table->string('location')->nullable();
             $table->enum('gender', ['male', 'female'])->nullable();
@@ -25,7 +26,6 @@ return new class extends Migration
             $table->text('description')->nullable();
             $table->text('bio')->nullable();
             $table->string('profile_picture')->nullable();
-            $table->enum('role', ['user', 'admin'])->default('user');
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
